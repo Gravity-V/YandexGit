cc185ce1edd10712e2bffb3f51154abb68358be5
Gravity-V <99959185+Gravity-V@users.noreply.github.com>
2024-11-30 19:33:35 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ def main():
     print('My first git program')
     print('And i change it every day')
     print('Again')
-    print('UFO came and added this line')
 
 if __name__ == '__main__':
     main()
